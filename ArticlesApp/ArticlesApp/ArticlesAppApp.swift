60e4b1e05e7d90ce7f5d47fdd1f6ab3774916dcf
Abdullah Ansari <iabdullahansari018@gmail.com>
2024-10-05 12:09:50 +0530

    setup

---
+++ ArticlesApp/ArticlesApp/ArticlesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArticlesAppApp.swift
+//  ArticlesApp
+//
+//  Created by Abdullah Ansari on 05/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ArticlesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
