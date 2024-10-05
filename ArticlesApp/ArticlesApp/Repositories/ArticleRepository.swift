538accbab86664efc4b86fc3a2e2e228b1deda40
Abdullah Ansari <iabdullahansari018@gmail.com>
2024-10-05 12:36:45 +0530

    implement the article repository

---
+++ ArticlesApp/ArticlesApp/Repositories/ArticleRepository.swift
@@ -0,0 +1,35 @@
+//
+//  ArticleRepository.swift
+//  ArticlesApp
+//
+//  Created by Abdullah Ansari on 05/10/24.
+//
+
+import Foundation
+
+// Article repository protocol
+protocol ArticleRepositoryProtocol {
+    func fetchArticles() async throws -> [Article]
+}
+
+// Article repository implementation
+class ArticleRepository: ArticleRepositoryProtocol {
+    private let networkService: NetworkServiceProtocol
+    private let apiKey = "9Mt5qvzmuHZ3HhUaTXGsmOptj6S9u16Q"
+
+    init(networkService: NetworkServiceProtocol = NetworkService()) {
+        self.networkService = networkService
+    }
+
+    func fetchArticles() async throws -> [Article] {
+        let parameters = ["api-key": apiKey]
+        let endPoint = EndPointBuilder()
+            .setPath(.getArticles)
+            .setMethod(.get)
+            .setBaseURL(.baseURL)
+            .setParameters(parameters)
+            .build()
+        let response: ArticlesResponse = try await networkService.request(endPoint: endPoint)
+        return response.results ?? []
+    }
+}
