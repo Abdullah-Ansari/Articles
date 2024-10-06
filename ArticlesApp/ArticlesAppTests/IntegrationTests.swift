c27bb647104cc00aa8196a78a20daf03b8e9cf7d
Abdullah Ansari <iabdullahansari018@gmail.com>
2024-10-06 16:57:27 +0530

    implement the integrtion test.

---
+++ ArticlesApp/ArticlesAppTests/IntegrationTests.swift
@@ -0,0 +1,51 @@
+//
+//  IntegrationTests.swift
+//  ArticlesAppTests
+//
+//  Created by Abdullah Ansari on 06/10/24.
+//
+
+import XCTest
+@testable import ArticlesApp
+
+final class IntegrationTests: XCTestCase {
+
+    var viewModel: ArticlesViewModel!
+    var repository: ArticleRepository!
+    var networkService: NetworkService!
+    
+    @MainActor 
+    override func setUp() {
+        networkService = NetworkService()
+        repository = ArticleRepository(networkService: networkService)
+        viewModel = ArticlesViewModel(repository: repository)
+    }
+    
+    override func tearDown() {
+        repository = nil
+        networkService = nil
+        viewModel = nil
+    }
+    
+    func testFetchArticlesFromServerSuccessful() async {
+        
+        let fetchTask = Task { await viewModel.fetchArticles() }
+        
+        // Yield control to allow any pending updates
+        await Task.yield()
+        
+        // Assert loading state is true before completion
+        await MainActor.run {
+            XCTAssertTrue(viewModel.isLoading, "Loading state should be true while fetching articles.")
+        }
+        
+        // Wait for the task to complete
+        await fetchTask.value
+        
+        // Assert loading state is false after completion
+        await MainActor.run {
+            XCTAssertFalse(viewModel.isLoading, "Loading state should be false after fetching articles.")
+        }
+    }
+
+}
